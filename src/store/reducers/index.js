332d1bcf5fbfa4aeda952aab4fe88366c4f09ede
roy <royhanley8@gmail.com>
2020-03-27 17:15:12 +0000

    Added register reducer

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux'
 
+import registerReducer from './register'
 import tokenReducer from './token'
 import userReducer from './user';
 
 export default combineReducers({
+  register: registerReducer,
   token: tokenReducer,
   user: userReducer,
 })
