578a3d32f728d976ff3e33b1c6e6a8c9b8a1ef5c
roy <royhanley8@gmail.com>
2020-03-27 16:24:18 +0000

    Root component can now detect if on mobile

--- src/store/components/Root/index.js
+++ src/store/components/Root/index.js
@@ -2,9 +2,37 @@ import React, {Component} from 'react';
 
 class Root extends Component {
 
+  state = {
+    screenWidth: 0,
+    screenHeight: 0,
+  };
+
+  componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener('resize', this.updateWindowDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindowDimensions);
+  }
+
+  updateWindowDimensions = () => {
+    this.setState({
+      screenWidth: window.innerWidth,
+      screenHeight: window.innerHeight,
+    });
+  };
+
+  getIsMobile() {
+    return this.state.screenWidth < 900
+  }
+
   render() {
+    const isMobile = this.getIsMobile();
     return (
-      <p>{"Placeholder"}</p>
+      <div>
+        {!isMobile ? (<p>{"desktop"}</p>) : (<p>{"mobile"}</p>)}
+      </div>
     )
   }
 
