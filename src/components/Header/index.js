26f421794ce626e0e0c6bdaffb09ce07086ee4fd
roy <royhanley8@gmail.com>
2020-03-28 13:54:59 +0000

    Remove menu button from Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,8 +4,6 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import HeaderWrapper from "./wrapper";
 
 const useStyles = makeStyles(theme => ({
@@ -33,14 +31,6 @@ const Header = (props) => {
     <div className={classes.root}>
       <AppBar position={"static"}>
         <Toolbar>
-          <IconButton
-            edge={"start"}
-            className={classes.menuButton}
-            color={"inherit"}
-            aria-label={"menu"}
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography variant={"h6"} className={classes.title}>
             {"FixTheNews"}
           </Typography>
