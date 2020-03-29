6390b2748a094fe13bc885d177ed30cb1782dbc0
roy <royhanley8@gmail.com>
2020-03-29 18:10:57 +0100

    Turning off react strict mode to hide material UI warning. I've left a comment in the file

--- src/index.js
+++ src/index.js
@@ -4,10 +4,22 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// TODO - Material UI throws "Warning: findDOMNode is deprecated in StrictMode.
+//  findDOMNode was passed an instance of LoadingSpinner which is inside
+//  StrictMode. Instead, add a ref directly to the element you want to
+//  reference. Learn more about using refs safely here:
+//  https://fb.me/react-strict-mode-find-node".
+//  I'm turning off StrictMode for now
+// With StrictMode
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
 
