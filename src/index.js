0fc7e2b95558b7ac635b99752e7a104e779967d4
roy <royhanley8@gmail.com>
2020-03-30 14:22:05 +0100

    Remove src/serviceWorker.js and src/setupTests.js files

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 // TODO - Material UI throws "Warning: findDOMNode is deprecated in StrictMode.
 //  findDOMNode was passed an instance of LoadingSpinner which is inside
@@ -22,8 +21,3 @@ ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
