7c369fbddec478cc0e25df6f3101e3527d3afda1
roy <royhanley8@gmail.com>
2020-04-05 13:32:04 +0100

    NewsItems components added

---
+++ src/components/NewsItems/NewsItemsContainer.js
@@ -0,0 +1,59 @@
+import React from "react";
+import {makeStyles} from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import NewsItem from "../NewsItem";
+import {capitalizeFirstLetter} from "../../helpers/stringFunctions";
+
+const useStyles = makeStyles((theme) => ({
+  newsItemsContainer: {
+    display: "flex",
+    marginTop: theme.spacing(1),
+  },
+  newsItemContainer: {
+    flex: 1,
+    padding: theme.spacing(1),
+  },
+  for: {
+    color: "green",
+  },
+  neutral: {
+    color: "orange",
+  },
+  against: {
+    color: "red",
+  },
+}));
+
+const NewsItemsContainer = ({topic}) => {
+  const classes = useStyles();
+  return (
+    <div className={classes.newsItemsContainer}>
+      {topic.categories.map(category => (
+        <div
+          key={`news-item-${category.id}`}
+          className={classes.newsItemContainer}
+        >
+          <Typography
+            className={classes[category.type]}
+            variant={"subtitle1"}
+          >
+            {capitalizeFirstLetter(category.title || category.type)}
+          </Typography>
+          <hr className={classes[category.type]} />
+          <div>
+            {topic.top_news_items[category.type].map(newsItem => (
+              <NewsItem
+                key={`news-item-${newsItem.id}`}
+                newsItem={newsItem}
+              />
+              ))
+            }
+          </div>
+        </div>
+        ))
+      }
+    </div>
+  )
+};
+
+export default NewsItemsContainer;
