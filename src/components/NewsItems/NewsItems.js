d9441983617665f81554bf4aaeb8e358cc51f0f0
roy <royhanley8@gmail.com>
2020-04-09 19:19:06 +0100

    Added NewsItems

---
+++ src/components/NewsItems/NewsItems.js
@@ -0,0 +1,8 @@
+import NeverEndingScrolling from "../NeverEndingScrolling";
+import NewsItem from "../NewsItem";
+
+class NewsItems extends NeverEndingScrolling {
+  ITEM_COMPONENT = NewsItem;
+}
+
+export default NewsItems;
