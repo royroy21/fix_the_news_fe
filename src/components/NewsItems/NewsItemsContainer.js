52a6af8656901b5949a3a6e888f8bd63eaf59440
roy <royhanley8@gmail.com>
2020-04-09 16:03:35 +0100

    Extra styling added to news items container

--- src/components/NewsItems/NewsItemsContainer.js
+++ src/components/NewsItems/NewsItemsContainer.js
@@ -5,13 +5,22 @@ import NewsItem from "../NewsItem";
 import {capitalizeFirstLetter} from "../../helpers/stringFunctions";
 
 const useStyles = makeStyles((theme) => ({
-  newsItemsContainer: {
+  container: {
     display: "flex",
     marginTop: theme.spacing(1),
+  },
+  moreNewsItems: {
+    position: "absolute",
+    bottom: 0,
+    fontWeight: "bold",
   },
   newsItemContainer: {
     flex: 1,
     padding: theme.spacing(1),
+    position: "relative",
+  },
+  newsItemsContainer: {
+    paddingBottom: theme.spacing(3),
   },
   for: {
     color: "green",
@@ -27,7 +36,7 @@ const useStyles = makeStyles((theme) => ({
 const NewsItemsContainer = ({topic}) => {
   const classes = useStyles();
   return (
-    <div className={classes.newsItemsContainer}>
+    <div className={classes.container}>
       {topic.serialized_categories.map(category => (
         <div
           key={`news-item-${category.id}`}
@@ -40,7 +49,7 @@ const NewsItemsContainer = ({topic}) => {
             {capitalizeFirstLetter(category.title || category.type)}
           </Typography>
           <hr className={classes[category.type]} />
-          <div>
+          <div className={classes.newsItemsContainer}>
             {topic.top_news_items[category.type].map(newsItem => (
               <NewsItem
                 key={`news-item-${newsItem.id}`}
@@ -49,6 +58,11 @@ const NewsItemsContainer = ({topic}) => {
               ))
             }
           </div>
+          {topic.news_items_count[category.type] > 3 ? (
+            <p className={classes.moreNewsItems}>
+              {`show ${topic.news_items_count[category.type]} more`}
+            </p>
+          ) : null}
         </div>
         ))
       }
