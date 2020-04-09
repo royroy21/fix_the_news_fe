527bdece60f59fc2b7b5720bc4befaaf63f815fb
roy <royhanley8@gmail.com>
2020-04-09 16:04:28 +0100

    Added working links, chips and likes indicator to news item

--- src/components/NewsItem/index.js
+++ src/components/NewsItem/index.js
@@ -1,20 +1,75 @@
 import React from "react";
 import {makeStyles} from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
+import Link from '@material-ui/core/Link';
+import Chip from "@material-ui/core/Chip";
+import ThumbUpAltOutlinedIcon from '@material-ui/icons/ThumbUpAltOutlined';
 
 const useStyles = makeStyles((theme) => ({
-  container: {
+  chip: {
+    cursor: "pointer",
+    "&:hover": {
+      color: "blue"
+    },
+  },
+  chipContainer: {
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    width: "100%",
+  },
+  link: {
+    color: "black",
+    "&:hover": {
+      color: "blue"
+    },
+  },
+  mainContainer: {
     marginBottom: theme.spacing(2),
+  },
+  thumbsUpCount: {
+    fontSize: "0.8em",
+    marginTop: 5,
+    marginLeft: theme.spacing(0.5),
+  },
+  thumbsUpIcon: {
+    color: "grey",
+    marginLeft: theme.spacing(1),
   },
 }));
 
 const NewsItem = ({newsItem}) => {
   const classes = useStyles();
   return (
-    <div className={classes.container}>
-      <Typography variant={"subtitle2"}>
-        {newsItem.title}
-      </Typography>
+    <div className={classes.mainContainer}>
+      <Link
+        className={classes.link}
+        href={newsItem.url}
+        target={"_blank"}
+        underline={"none"}
+      >
+        <Typography variant={"subtitle2"}>
+          {newsItem.title}
+        </Typography>
+      </Link>
+      <div className={classes.chipContainer}>
+        <Link
+          className={classes.link}
+          href={newsItem.url}
+          target={"_blank"}
+          underline={"none"}
+        >
+          <Chip
+            className={classes.chip}
+            size={"small"}
+            label={newsItem.serialized_type.title}
+          />
+        </Link>
+        <ThumbUpAltOutlinedIcon
+          className={classes.thumbsUpIcon}
+        />
+        <span className={classes.thumbsUpCount}>{"99"}</span>
+      </div>
     </div>
   )
 };
