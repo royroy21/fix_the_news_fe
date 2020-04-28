eff9b8202198cdbe43ee2b5255e2d49e8bb2b4b9
roy <royhanley8@gmail.com>
2020-04-28 16:35:25 +0100

    Added news item store

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux'
 
 import appDimensionsReducer from './appDimensions'
 import categoryNewsItemsReducer from './categoryNewsItems'
+import newsItemReducer from './newsItem'
 import registerReducer from './register'
 import tokenReducer from './token'
 import topicsReducer from './topics'
@@ -10,6 +11,7 @@ import userReducer from './user';
 export default combineReducers({
   appDimensions: appDimensionsReducer,
   categoryNewsItems: categoryNewsItemsReducer,
+  newsItem: newsItemReducer,
   register: registerReducer,
   token: tokenReducer,
   topics: topicsReducer,
