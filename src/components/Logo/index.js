4be02f5a6414122ce8a2413332fcba1a0448fba4
roy <royhanley8@gmail.com>
2020-05-02 16:14:43 +0100

    Added Logo component

---
+++ src/components/Logo/index.js
@@ -0,0 +1,39 @@
+import fixTheNewsLogo07 from '../../images/fixTheNewsLogo07.svg';
+import React from "react";
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    flexGrow: 1,
+  },
+  image: {
+    height: 40,
+    width: 40,
+  },
+  textContainer: {
+    fontSize: 30,
+    fontWeight: "bold",
+    marginLeft: theme.spacing(0.5),
+    marginTop: 4,
+  },
+  text: {
+    color: "#FF4500",
+  },
+}));
+
+const Logo = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.container}>
+      <img className={classes.image} src={fixTheNewsLogo07} alt="??" />
+      <div className={classes.textContainer}>
+        <span className={classes.text}>{"fix"}</span>
+        <span>{"thenews"}</span>
+      </div>
+    </div>
+  )
+};
+
+export default Logo;
