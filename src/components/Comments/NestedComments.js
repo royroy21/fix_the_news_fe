44ef1b25d42275fdd6886c797f5ac8d16e022cbd
roy <royhanley8@gmail.com>
2020-06-01 14:36:39 +0100

    Added NestedComments

---
+++ src/components/Comments/NestedComments.js
@@ -0,0 +1,19 @@
+import React from "react";
+import NeverEndingScrolling from "../NeverEndingScrolling";
+import {initialCommentCommentsState} from "../../store/reducers/commentComments";
+import Comment from '../Comment';
+
+const NestedComments = ({actions, item, store}) => {
+  return (
+    <NeverEndingScrolling
+      getInitialRequest={() => actions.getCommentComments({comment: item.id})}
+      getNext={actions.getCommentComments}
+      id={`comments-for-comment-${item.id}`}
+      ItemComponent={Comment}
+      store={store.commentComments[item.id] || initialCommentCommentsState}
+      style={{maxHeight: 400}}
+    />
+  )
+};
+
+export default NestedComments;
