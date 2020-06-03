7ccf358c6dec41ff59aa9ae6b2ba4fbca676fc23
roy <royhanley8@gmail.com>
2020-06-03 15:05:17 +0100

    Added extra margins at comment button for mobile

--- src/components/Comments/CommentsButton.js
+++ src/components/Comments/CommentsButton.js
@@ -42,7 +42,7 @@ const CommentsButton = ({actions, commentsCount, store, topicId}) => {
   return (
     <div
       className={classes.container}
-      style={store.appDimensions.isMobile ? {marginRight: 10} : undefined}
+      style={store.appDimensions.isMobile ? {marginLeft: 5, marginRight: 5} : undefined}
     >
       <div
         className={classes.content}
