57d1e3c4f936e4fa3cec8e563d6712e844498d57
roy <royhanley8@gmail.com>
2020-06-04 15:13:18 +0100

    Added ListPaginationWithMoreButton

---
+++ src/components/ListPaginationWithMoreButton/index.js
@@ -0,0 +1,57 @@
+import React, {Fragment, useEffect} from "react";
+import LoadingSpinner from "../Loading/LoadingSpinner";
+import {parseURL} from "../../helpers/urlFunctions";
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  more: {
+    color: theme.palette.secondary.main,
+    cursor: 'pointer',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    '&:hover': {
+      color: theme.palette.secondary.light,
+    },
+  },
+}));
+
+const ListPaginationWithMoreButton = ({ItemComponent, request, requestParamName, requestParamValue, store}) => {
+  const classes = useStyles();
+
+  useEffect(
+    () => {request({[requestParamName]: requestParamValue})},
+    [request, requestParamName, requestParamValue],
+  );
+
+  const getNextURL = () => {
+    if (!store.loading) {
+      const {next: nextURL} = (store.objects || {});
+      return nextURL;
+    }
+  };
+
+  const getNextItems = () => {
+    const nextURL = getNextURL();
+    if (nextURL) {
+      const {params} = parseURL(nextURL);
+      request(params);
+    }
+  };
+
+  return (
+    <Fragment>
+      {store.items.map(item => (
+      <ItemComponent
+        key={`item-component-${item.id}`}
+        item={item}
+      />
+      ))}
+      {store.loading && <LoadingSpinner />}
+      {getNextURL() ? (
+        <div className={classes.more} onClick={getNextItems}>{"more"}</div>
+      ) : null}
+    </Fragment>
+  )
+};
+
+export default ListPaginationWithMoreButton;
