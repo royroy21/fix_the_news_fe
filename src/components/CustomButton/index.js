c5af79fd0c1c06e72855e4077070fc97e92f71ce
roy <royhanley8@gmail.com>
2020-06-05 12:31:40 +0100

    CustomButton returns IconButton if not icon is provided with no label

--- src/components/CustomButton/index.js
+++ src/components/CustomButton/index.js
@@ -1,21 +1,35 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
 
 const CustomButton = ({icon=undefined, inverted=false, label='', onClick=undefined, style={}}) => {
   return (
-    <Button
-      color={'secondary'}
-      children={label || ""}
-      onClick={onClick}
-      startIcon={icon}
-      style={{
-        ...style,
-        borderRadius: 25,
-        height: 36,
-        maxWidth: 250,
-      }}
-      variant={inverted ? 'outlined' : 'contained'}
-    />
+    icon && !label ? (
+      <IconButton
+        color={'secondary'}
+        onClick={onClick}
+        style={{
+          ...style,
+          padding: 6,
+        }}
+      >
+        {icon}
+      </IconButton>
+    ) : (
+      <Button
+        color={'secondary'}
+        children={label || ""}
+        onClick={onClick}
+        startIcon={icon}
+        style={{
+          ...style,
+          borderRadius: 25,
+          height: 36,
+          maxWidth: 250,
+        }}
+        variant={inverted ? 'outlined' : 'contained'}
+      />
+    )
   )
 };
 
