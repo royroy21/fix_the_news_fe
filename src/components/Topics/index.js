8cc673e843fa6fc6bda16b16deef73192e20c77e
roy <royhanley8@gmail.com>
2020-06-25 18:17:24 +0100

    Topics component now uses TOPIC_PAGE_SIZE_FOR_MOBILE

--- src/components/Topics/index.js
+++ src/components/Topics/index.js
@@ -2,6 +2,7 @@ import Topic from "../Topic";
 import React from "react";
 import TopicsWrapper from "./wrapper";
 import NeverEndingScrolling from "../NeverEndingScrolling";
+import {TOPIC_PAGE_SIZE_FOR_MOBILE} from "../../settings";
 
 const Topics = ({id, actions, store, style={}}) => {
   if (store.appDimensions.isMobile === null) {
@@ -9,7 +10,9 @@ const Topics = ({id, actions, store, style={}}) => {
   }
 
   const getInitialRequest = () => {
-    const params = store.appDimensions.isMobile ? {size: 3} : {};
+    const params = store.appDimensions.isMobile
+      ? {size: TOPIC_PAGE_SIZE_FOR_MOBILE}
+      : {};
     actions.getTopics(params);
   };
   return (
