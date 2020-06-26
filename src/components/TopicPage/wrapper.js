0bb41391ee5c51264ba29b0115033a228bb9bf70
roy <royhanley8@gmail.com>
2020-06-26 16:24:26 +0100

    Added TopicPage component

---
+++ src/components/TopicPage/wrapper.js
@@ -0,0 +1,32 @@
+import { connect } from 'react-redux'
+import {getTopic} from "../../store/actions/topic";
+import {prependTopic} from "../../store/actions/topics";
+
+const mapStateToProps = (state) => {
+  return {
+    store: {
+      appDimensions: state.appDimensions,
+      topic: state.topic,
+    },
+  }
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    actions: {
+      getTopic: slug => {
+        dispatch(getTopic(slug))
+      },
+      prependTopic: topic => {
+        dispatch(prependTopic(topic))
+      },
+    }
+  }
+};
+
+const TopicPageWrapper = connect(
+  mapStateToProps,
+  mapDispatchToProps
+);
+
+export default TopicPageWrapper;
