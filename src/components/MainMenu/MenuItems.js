4ae55a9a7a1fb5220df8da32a1f1dd6513259146
roy <royhanley8@gmail.com>
2020-06-28 15:48:10 +0100

    Fix main menu not closing bug

--- src/components/MainMenu/MenuItems.js
+++ src/components/MainMenu/MenuItems.js
@@ -14,6 +14,7 @@ export const useStyles = makeStyles(theme => ({
     height: "90%",
     paddingLeft: theme.spacing(1),
     position: "relative",
+    width: "80%",
   },
   link: {
     color: theme.palette.primary.dark,
