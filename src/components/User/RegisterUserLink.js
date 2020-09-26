1942d522a0414bf67acf6edc30db6b85c97dfb17
Roy Hanley <royhanley8@gmail.com>
2020-09-26 13:28:10 +0100

    Change to registration message

--- src/components/User/RegisterUserLink.js
+++ src/components/User/RegisterUserLink.js
@@ -1,12 +1,27 @@
 import React from "react";
 import ButtonForModal from "../CustomButton/ButtonForModal";
 import RegistrationModal from "./RegistrationModal";
+import {themeObject} from "../../theme";
+
+
 
 const RegisterUserLink = () => {
+  const messageStyle ={
+    fontSize: "1.3em",
+  }
+  const signUpStyle = {
+    color: themeObject.palette.secondary.main,
+    fontSize: "1.3em",
+  }
   return (
     <ButtonForModal
       buttonAsLink={true}
-      label={"register new user"}
+      label={
+        <div>
+          <span style={messageStyle}>{"Don't have an account?  "}
+          </span><span style={signUpStyle}>{"Sign up"}</span>
+        </div>
+      }
       inverted={true}
       Modal={RegistrationModal}
     />
